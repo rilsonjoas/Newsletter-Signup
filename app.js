628752f92242a45bcef6ba90d22422ae01230548
Rilson Joás <107469259+narniano@users.noreply.github.com>
2023-07-09 18:51:08 -0300

    Updating index.html

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/signup.html");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/", (req, res) => {
